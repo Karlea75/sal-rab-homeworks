2323dd60846331272effb79c0fa145eb8959ee57
Kate <kathy_de-sprite@bk.ru>
2022-07-05 12:49:58 +0300

    f

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,7 +5,7 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
-    let shippingSum;
+    let shippingSum = shippingSum;
     if(productsSum = 0 || productsSum >= freeShippingMinSum){
         shippingSum = 0;
     } else if(productsSum > 0 && productsSum < freeShippingMinSum){
