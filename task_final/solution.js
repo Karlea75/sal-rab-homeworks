3080f3c5953affd3854d70618fd47db82a9e06bf
Kate <kathy_de-sprite@bk.ru>
2022-07-06 14:32:50 +0300

    aaaaaaaaaaaaa

--- task_final/solution.js
+++ task_final/solution.js
@@ -57,8 +57,8 @@ function sendRequest(name, phone, address, goods, sum) {
 let products = [];
 //let address = `${street} , ${house} , ${entrance} , ${floor} , ${flat}`;
 
-let data = {client: `${name} ${phone}`, order: {address: address, sum: sum}, goods: products};
-    
+let data = {client: `${name} ${phone}`, order: {address: addressNormal, sum: sum}, goods: products};
+    addressNormal = `${address.street} , ${address.house} , ${address.entrance} , ${address.floor} , ${address.flat}`;
 let length = goods.length;
     for (let i = 0; i < length; i += 1) {
         let product = {title: goods[i].title, count: goods[i].count};
